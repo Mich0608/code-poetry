2454588132bb753efb8f04224d53d1322a7b30c1
Mich0608 <mjtchen68@gmail.com>
2021-04-11 22:03:27 -0500

    ..adulthood?

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o puberty puberty.c
---
+++ puberty.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void puberty1(int age)
+{
+    if(age == 16)
+        return;
+
+    char *puberty[2];
+    puberty[0] = "      @dolescence++\n}";
+    puberty[1] = "      return ....gr0wn;\n}";  
+
+    printf("while(age == %d)\n{\n   puberty1();\n", age);
+    printf("%s\n", puberty[age % 2]);
+
+    age++;
+    puberty1(age);
+}
+
+void puberty2()
+{
+    printf("now(I == 19 == almost 20[?]){   I believe this is\n");
+    for(int I = 14; I < 20; I++)
+    {
+        for(int i = 13; i < I; i++)
+        {
+            printf("      ");
+        }
+        printf("puberty2");
+        printf("...\n");
+    }
+    printf("}\n");
+}
+
+int main()
+{
+    int age = 12;
+    puberty1(age);
+    puberty2();
+    printf("\n..adulthood?\n\n");
+    return 0;
+}
